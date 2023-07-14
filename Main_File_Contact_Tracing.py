be92e6b5bcdb3c8fce9a163566323a58d8d520ac
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-15 01:34:16 +0800

    Main file with pseudocode

---
+++ Main_File_Contact_Tracing.py
@@ -0,0 +1,9 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Final Project in OOP
+#Create a Covid-19 Contact Tracing App with GUI
+
+#Create Class GUI
+#create def for constructor
+#create def for Add Entry
+#Create def for Search Entry
+#Executes code within main
